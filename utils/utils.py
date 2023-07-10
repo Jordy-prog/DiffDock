03a41791ac9589edaeba864ce1feb2b4c0ae407d
Jordy <jordyschifferstein@hotmail.com>
2023-07-10 14:17:24 +0200

    Added test file

--- utils/utils.py
+++ utils/utils.py
@@ -118,7 +118,7 @@ def get_model(args, device, t_to_sigma, no_parallel=False, confidence_mode=False
                         num_confidence_outputs=len(
                             args.rmsd_classification_cutoff) + 1 if 'rmsd_classification_cutoff' in args and isinstance(
                             args.rmsd_classification_cutoff, list) else 1)
-
+    
     if device.type == 'cuda' and not no_parallel:
         model = DataParallel(model)
     model.to(device)
